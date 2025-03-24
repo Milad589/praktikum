1cef248a979bde7c071325aae7a275a5426db9b1
Sayed Milad Hussaini <miladhussaini641@gmail.com>
2025-03-24 12:31:04 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("hello world")
+#variable
+message = "hello world"
+print(message)
+#loop
+loopcount = 100
+for i in range(loopcount):
+    print(message)
+ #bedingung
+message = "Milad"
+if message == "hello world":
+    print("message is hello world")
+else:
+    print("message is not hello world")
