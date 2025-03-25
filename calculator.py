3fd24a72593e41135f0df6691754962353e89982
Milad589 <miladhussaini641@gmail.com>
2025-03-25 14:46:04 +0100

    Taschenrechner

---
+++ calculator.py
@@ -0,0 +1,48 @@
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def multi(x, y):
+    return x * y
+
+def divi(x, y):
+    if y == "0":
+        raise ZeroDivisionError
+    return x / y
+
+
+
+
+while True:
+    try:
+        a = float(input("Erste Zahl: "))
+        op = input("Operator (+, -, *, /): ")
+        b = float(input("Zweite Zahl: "))
+        
+        if op == "*":
+            result = multi(a, b)
+            print(result)
+        elif op == "+":
+            result = add(a, b)
+            print(result)
+        elif op == "-":
+            result = sub(a, b)    
+            print(result)
+        elif op == "/":
+            result = divi(a, b)
+            print(result)
+        else:
+            print("ungültiger Operator gültige Operator sind: +, -, *, /")    
+    except ZeroDivisionError:
+        print("Division durch null nicht möglcih")
+    except ValueError:
+        print("Bitte eine gültige Zahl eingeben.")
+
+
+
+
+
+
+    
