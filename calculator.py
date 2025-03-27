389cf612e2ef18c3e9307de9f88ad128c57eccc6
Milad589 <miladhussaini641@gmail.com>
2025-03-27 12:07:49 +0100

    Better version

--- calculator.py
+++ calculator.py
@@ -5,45 +5,52 @@ def sub(x, y):
     return x - y
 
 def multi(x, y):
-    return x * y
+    return x * y 
 
 def divi(x, y):
     if y == "0":
         raise ZeroDivisionError
-    return x / y
+    return compute
+
+def message(msg):
+    print("\n=== " + str(msg) + " ===\n")
 
 def compute(op, x, y):
+    #print(type(op), type(x), type(y))
     result = 0
     if op == "*":
-        resukt = multi(x, y)
+        result = multi(x, y)
     elif op == "+":
-        resukt = add(x, y)
+        result = add(x, y)
     elif op == "-":
         result = sub(x, y)
     elif op == "/":
         result = divi(x, y)
-        return result
-    else:
-        print("Ungültiger Operator gültige Operatoren sind +,-,/,*")
-    
-
-
-
-while True:
-    try:
-        op = input("Operator (+, -, *, /): ")
-        if op == "q":
-            exit(0)
-
-        a = float(input("Erste Zahl: "))
-        b = float(input("Zweite Zahl: "))
-        
-        
-        print("Das Ergebnis lautet:", compute(op, a, b))
-    except ZeroDivisionError:
-        print("Division durch null nicht möglcih")
-    except ValueError:
-        print("Bitte eine gültige Zahl eingeben.")
+    return result
+ 
+def main():
+    while True:
+        try:
+            erlaubte_operatoren = ["+","-","*","/"]
+            op = input("Operator (+, -, *, /) or q for leave: ")
+            if op == "q":
+                message("Programm Beendet")
+                exit(0)
+ 
+            if op in erlaubte_operatoren:
+                a = float(input("Erste Zahl: "))
+                b = float(input("Zweite Zahl: "))
+                message("Das Ergebnis lautet: " + str(result))
+            else:
+                message("Ungültiger Operator gültige Operatoren sind +,-,/,*")
+       
+        except ZeroDivisionError:
+            message("Division durch null nicht möglcih")
+        except ValueError:
+            message("Bitte eine gültige Zahl eingeben.")
+ 
+if __name__ == '__main__':
+    main()
 
 
 
