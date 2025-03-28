b9b999c83608c7f27fb2f564a773c2e64d488ee6
Milad589 <miladhussaini641@gmail.com>
2025-03-28 14:05:28 +0100

    Docs hinzu gefügt

--- mylog.py
+++ mylog.py
@@ -1,12 +1,19 @@
 import logging
 
 def mylog(logfilename):
-    logging.basicConfig(level=logging.INFO, filename=logfilename, filemode='w',format='%(asctime)s - %(levelname)s - %(message)s')
+    """
+    Konfiguriert das Logging-Modul mit einem gegebenen Log-Dateinamen.
+    """
+    logging.basicConfig(level=logging.INFO, filename=logfilename, filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def message(Loglevel, msg):
     """
-    Loggt eine Nachricht mit dem angegebenen Log-Level.
+    Loggt eine Nachricht mit dem angegebenen Log-Level (info, error, warning, debug).
+    
+    Args:
+        Loglevel (str): Das Log-Level (info, error, warning, debug).
+        msg (str): Die Nachricht, die geloggt werden soll.
     """
     loglevel = Loglevel.lower()
     if loglevel == "info":
--- rechnen.py
+++ rechnen.py
@@ -2,7 +2,20 @@ import logging
 from mylog import message
 
 def compute(op, a, b):
-    """Führt eine Berechnung durch und loggt das Ergebnis."""
+    """
+    Führt eine Berechnung basierend auf dem Operator und den Operanden durch.
+
+    Args:
+        op (str): Der Operator, der die Art der Berechnung angibt (+, -, *, /).
+        a (float): Der erste Operand.
+        b (float): Der zweite Operand.
+
+    Returns:
+        float: Das Ergebnis der Berechnung.
+
+    Raises:
+        ZeroDivisionError: Wenn der Operator "/" ist und b gleich 0 ist.
+    """
     message("info", "Berechne: " + str(a) + " " + op + " " + str(b))
 
     if op == "+":
